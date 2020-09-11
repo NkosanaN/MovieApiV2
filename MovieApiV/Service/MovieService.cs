b6117f3fc2f26378f061f0ac4ce6d57e70fe915d
Nkosana Ndlela <nkosana.prince.ndlela@gmail.com>
2020-09-11 23:43:16 +0200

    added

--- MovieApiV/Service/MovieService.cs
+++ MovieApiV/Service/MovieService.cs
@@ -10,12 +10,11 @@ namespace MovieApiV.Services
 {
     public partial class DataHandler
     {
-       // public Utils Util { get; set; }
+        // public Utils Util { get; set; }
         public async Task<List<Movie>> MovieListGet()
         {
             var sql = "exec sp_movie_data 0, 1";
 
-            //var dt = Util.Select(sql);
             var dt = Util.Select(sql);
             var list = new List<Movie>();
 
@@ -30,5 +29,35 @@ namespace MovieApiV.Services
             }
             return list;
         }
+        public Movie MovieGetSingle(int Id)
+        {
+            var sql = $"exec sp_movie_data 1, @id={Id}";
+
+            var dt = Util.Select(sql);
+            if (dt.Rows.Count == 0)
+            {
+                return null;
+            }
+
+            DataRow row = dt.Rows[0];
+            return new Movie
+            {
+                Genre = row["Genre"].ToString(),
+                ReleaseDate = (DateTime)row["ReleaseDate"],
+                Title = row["Genre"].ToString()
+            };
+        }
+        public bool AddMovie(Movie data)
+        {
+            var sql = $"exec sp_users_process 0 , @Title='{data.Title}',@Genre = '{data.Genre}'";
+            return Util.Execute(sql);
+        }
+        //must test this one
+        public bool DeleteMovie(int Id)
+        {
+            var sql = $"exec sp_movie_data 2 @id={Id}";
+            return Util.Execute(sql);
+        }
+
     }
 }
